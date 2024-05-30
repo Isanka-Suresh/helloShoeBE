f0b571d0907ce589382b7ebe5572c20de7d1e436
Isanka Suresh <isankasuresh001@gmail.com>
2024-05-30 22:38:02 +0530

    updated Customer controller

--- src/main/java/lk/ijse/helloshoebe/controller/CustomerController.java
+++ src/main/java/lk/ijse/helloshoebe/controller/CustomerController.java
@@ -3,9 +3,14 @@ package lk.ijse.helloshoebe.controller;
 import lk.ijse.helloshoebe.dto.AlertDTO;
 import lk.ijse.helloshoebe.dto.CustomerDTO;
 import lk.ijse.helloshoebe.entity.Customer;
+import lk.ijse.helloshoebe.exception.DuplicateException;
+import lk.ijse.helloshoebe.exception.NotFoundException;
 import lk.ijse.helloshoebe.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,38 +20,94 @@ import java.util.List;
 @RequestMapping(value = "api/v1/customer")
 @RequiredArgsConstructor
 @CrossOrigin
+@Slf4j
 public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
 
-    @GetMapping("/getAllCustomer")
-    public ResponseEntity<List<CustomerDTO>> getAllCustomer(){
-        System.out.println("Requested");
-        return ResponseEntity.ok(customerService.getAllCustomers());
+    //Checking the Customer Controller
+    @GetMapping("/health")
+    public String customerHealthCheck() {
+        return "Customer Working";
+
     }
 
-    @GetMapping("/getCustomer/{customerId}")
-    public ResponseEntity<CustomerDTO> getCustomer(@PathVariable("customerId")String custId){
-        return ResponseEntity.ok(customerService.getSelectedCustomer(custId));
+    //Get All Customers
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<CustomerDTO>> getAllCustomer() {
+        log.info("Selecting All Customers");
+        try {
+            List<CustomerDTO> customerDTOList = customerService.getAllCustomers();
+            log.debug("Selected {} Customers Successfully", customerDTOList.size());
+            return ResponseEntity.status(HttpStatus.OK).body(customerDTOList);
+        } catch (NotFoundException e) {
+            log.error("Failed to find Customers -> {}", e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
     }
 
-    @PostMapping("/saveCustomer")
-    public ResponseEntity<CustomerDTO> saveCustomer(@RequestBody CustomerDTO customerDTO){
-        System.out.println("Save Customer Works");
-        return ResponseEntity.ok(customerService.saveCustomer(customerDTO));
+    //Get Selected Customer
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(value = "{customerId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CustomerDTO> getCustomer(@PathVariable("customerId") String customerId) {
+        log.info("looking for Customer {}", customerId);
+        System.out.println(customerId);
+        try {
+            CustomerDTO customerDTO = customerService.getSelectedCustomer(customerId);
+            log.debug("Found Customer {}", customerDTO.getCustomerId());
+            return ResponseEntity.status(HttpStatus.OK).body(customerDTO);
+        } catch (NotFoundException e) {
+            log.error("Customer {} don't Exist", customerId, e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
 
-    @PutMapping("/updateCustomer")
-    public ResponseEntity<CustomerDTO> updateCustomer(@RequestBody CustomerDTO customerDTO){
-        return ResponseEntity.ok(customerService.updateCustomer(customerDTO));
+    //Save Customer
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CustomerDTO> saveCustomer(@RequestBody CustomerDTO customerDTO) {
+        log.info("Adding new Customer -> {}", customerDTO);
+        try {
+            CustomerDTO savedCustomer = customerService.saveCustomer(customerDTO);
+            log.debug("Customer Added Successfully -> {}", savedCustomer);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedCustomer);
+        } catch (DuplicateException e) {
+            log.error("Failed to Add Customer {}", e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
 
-    @DeleteMapping("/deleteCustomer/{customerId}")
-    public ResponseEntity<String> deleteCustomer(@PathVariable("customerId")String custId){
-        customerService.deleteCustomer(custId);
-        return ResponseEntity.ok("Customer Deleted");
+    //Update Customer
+    @ResponseStatus(HttpStatus.CREATED)
+    @PutMapping(value = "{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CustomerDTO> updateCustomer(@RequestBody CustomerDTO customerDTO, @PathVariable("id") String customerId) {
+        log.info("Updating Customer {}", customerId);
+        customerDTO.setCustomerId(customerId);
+        try {
+            CustomerDTO savedCustomer = customerService.updateCustomer(customerDTO);
+            log.debug("Customer Updated Successfully -> {}", savedCustomer);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedCustomer);
+        } catch (NotFoundException | DuplicateException e) {
+            log.error("Failed to Update Customer {}", e.getMessage(), e);
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+    //Delete Customer
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity<String> deleteCustomer(@PathVariable("id") String customerId) {
+        log.info("Deleting Customer -> {}", customerId);
+        try {
+            customerService.deleteCustomer(customerId);
+            log.debug("Customer {} Deleted Successfully", customerId);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+        } catch (NotFoundException e) {
+            log.error("Failed to Delete Customer {}", e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
 }
-
-
