eb096d6a1a1033ff7e3ffd28635030cab373a110
Isanka Suresh <isankasuresh001@gmail.com>
2024-05-30 22:39:51 +0530

    implement InvalidDataException exception

---
+++ src/main/java/lk/ijse/helloshoebe/exception/InvalidDataException.java
@@ -0,0 +1,7 @@
+package lk.ijse.helloshoebe.exception;
+
+public class InvalidDataException extends RuntimeException {
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
